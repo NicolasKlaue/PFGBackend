a6cef3ac29ab3e040aa9ab29492666281f95fafb
Nicolas Klaue <klaue.nico@gmail.com>
2024-04-22 17:35:55 +0200

    Change email classification candidate labels and enable multi-label classification
    * Replace single 'Irrelevant' label with a list of specific labels
    * Enable multi-label classification for the `classifier` function
    * Update `candidateLabels

--- main.py
+++ main.py
@@ -31,7 +31,34 @@ async def RateEmail(email:Email) -> Urgency:
      {Subject}
      Body
      {Body}""".format(Subject = email.Subject, Body = email.Body)
-     candidateLabels = ['Irrelevant', 'Not Urgent', 'Mildly Urgent','Urgent','Extremely urgent']
-     classDict = classifier(sequence_to_classify, candidateLabels)
+     candidateLabels = ['energy infrastructure',
+                        'other',
+                        'government & politics',
+                        'legal affairs',
+                        'calendar & scheduling',
+                        'employment',
+                        'personal & social',
+                        'customer support',
+                        'business document',
+                        'energy trading',
+                        'project management',
+                        'meetings & events',
+                        'project-specific',
+                        'human resources',
+                        'contract management',
+                        'marketing & promotion',
+                        'information technology',
+                        'external affairs',
+                        'media & press' 'finance',
+                        'corporate governance',
+                        'energy services',
+                        'organization',
+                        'utilities',
+                        'phone communication',
+                        'safety & emergency',
+                        'market research',
+                        'email management',
+                        'education & training']
+     classDict = classifier(sequence_to_classify, candidateLabels,multi_label=True)
      urgency= Urgency(int(candidateLabels.index(classDict['labels'][0]) + 1 ),classDict['labels'][0])
      return urgency
