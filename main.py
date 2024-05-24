ebd79b6911f50d888e601fb943d8e636d184167d
Nicolas Klaue <klaue.nico@gmail.com>
2024-05-24 09:58:06 +0200

    Added tone classifier and MLP model, refactored email rating logic to use new models

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
 import configparser
 import json
+import joblib
+from keras.models import load_model
 classifier = pipeline("zero-shot-classification",
                       model="facebook/bart-large-mnli")
+
+# TONE CLASSIFIER
+classifierTone = joblib.load("tone_classifier_model.pkl")
+vectorizerTone = joblib.load("tfidf_vectorizer.pkl")
+
+# MLP CLASSIFIER
+MLPModel = load_model("ModeloMLP.keras", compile=False)
 
 # Create a ConfigParser object
 config = configparser.ConfigParser()
@@ -41,6 +50,49 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+def RateEmailTone(emailInput: Email) -> int:
+     # Email to predict
+     email = """
+          Subject:
+          {Subject}
+          Body:
+          {Body}""".format(Subject=emailInput.Subject, Body=emailInput.Body)
+     # Vectorize the email
+     email_vectorized = vectorizerTone.transform([email])
+
+     # Predict the tone of the email
+     predicted_tone = classifierTone.predict(email_vectorized)
+
+     toneClass = 1 if predicted_tone[0] == "formal" else - \
+     1 if predicted_tone[0] == "casual" else 0
+     return toneClass
+
+def RateEmailTopic(email: Email) -> Urgency:
+     sequence_to_classify = """
+          Subject
+          {Subject}
+          Body
+          {Body}""".format(Subject=email.Subject, Body=email.Body)
+
+     classDict = label_urgency_dict(sequence_to_classify, list(
+     label_urgency_dict.keys()), multi_label=True)
+     filtered_predictions = [(label, score) for label, score in zip(
+     classDict['labels'], classDict['scores']) if score > 0.3]
+
+     sorted_predictions = sorted(
+     filtered_predictions, key=lambda x: x[1], reverse=True)
+
+     top_3_classifications = [label for label, _ in sorted_predictions[:3]]
+
+     urgency_ratings = [label_urgency_dict[classification]
+                    for classification in top_3_classifications]
+     if not top_3_classifications:
+       top_3_classifications = ["other"]
+       urgency_rating = 0
+     else:
+       urgency_rating = max(urgency_ratings)
+     print("Still missing")
+     return {Urgency(urgency_rating,top_3_classifications)}
 
 @app.get("/")
 async def SendJSONTemplate():
@@ -69,25 +121,13 @@ async def send_json_template(config_data: ConfigType):
         return {"message": "Config updated successfully"}  # Return success message
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))  # Return error message with status code 500 for internal server error
+   
 @app.post("/")
 async def RateEmail(email:Email):
-     sequence_to_classify = """
-     Subject
-     {Subject}
-     Body
-     {Body}""".format(Subject = email.Subject, Body = email.Body)
-
-     classDict = classifier(sequence_to_classify, list(label_urgency_dict.keys()), multi_label=True)
-     print(classDict)
-     filtered_predictions = [(label, score) for label, score in zip(classDict['labels'], classDict['scores']) if score > 0.3]
-
-     sorted_predictions = sorted(filtered_predictions, key=lambda x: x[1], reverse=True)
-
-     top_3_classifications = [label for label, _ in sorted_predictions[:3]]
-
-     urgency_ratings = [label_urgency_dict[classification] for classification in top_3_classifications]
-     if not top_3_classifications:
-        top_3_classifications = ["other"]
-        urgency_ratings=0
-     urgency_rating = max(urgency_ratings)
-     return {Urgency(urgency_rating,top_3_classifications)}
+          TopicUrgency = RateEmailTopic(email)
+          if (TopicUrgency == 5) or (TopicUrgency == 0):
+               return TopicUrgency
+          TopicUrgency.urgencyRating = TopicUrgency.urgencyRating/5
+          ToneUrgency = RateEmailTone(email)/5
+          TopicUrgency.urgencyRating = MLPModel.predict([TopicUrgency.urgencyRating,ToneUrgency])
+          return {TopicUrgency}
